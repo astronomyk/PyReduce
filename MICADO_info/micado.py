a8047e6b8155390928a5561d5598da848e3e0b8c
Kieran <kdleschinski@gmail.com>
2021-06-09 11:20:31 +0200

    new values for the pyreduce-fits mapping file (micado.json)

--- MICADO_info/micado.py
+++ MICADO_info/micado.py
@@ -12,33 +12,34 @@ import numpy as np
 from astropy.io import fits
 from dateutil import parser
 
-from .common import getter, InstrumentWithModes, observation_date_to_night
+from ..pyreduce.instruments.common import getter, InstrumentWithModes, Instrument, observation_date_to_night
 
 logger = logging.getLogger(__name__)
 
 
-class XSHOOTER(InstrumentWithModes):
+class MICADO(Instrument):
     def add_header_info(self, header, mode, **kwargs):
         """ read data from header and add it as REDUCE keyword back to the header """
         # "Normal" stuff is handled by the general version, specific changes to values happen here
         # alternatively you can implement all of it here, whatever works
         header = super().add_header_info(header, mode)
 
-        header["e_backg"] = (
-            header["e_readn"] + header["e_exptime"] * header["e_drk"] / 3600
-        )
-
-        header["e_ra"] /= 15
-        if header["e_jd"] is not None:
-            header["e_jd"] += header["e_exptime"] / 2 / 3600 / 24 + 0.5
+        # header["e_backg"] = (
+        #     header["e_readn"] + header["e_exptime"] * header["e_drk"] / 3600
+        # )
+        #
+        # header["e_ra"] /= 15
+        # if header["e_jd"] is not None:
+        #     header["e_jd"] += header["e_exptime"] / 2 / 3600 / 24 + 0.5
 
         return header
 
     def get_wavecal_filename(self, header, mode, **kwargs):
         """ Get the filename of the wavelength calibration config file """
-        info = self.load_info()
+        # info = self.load_info()
+        # cwd = os.path.dirname(__file__)
+        # fname = f"xshooter_{mode.lower()}.npz"
+        # fname = os.path.join(cwd, "..", "wavecal", fname)
+        fname = "micado.npz"
 
-        cwd = os.path.dirname(__file__)
-        fname = f"xshooter_{mode.lower()}.npz"
-        fname = os.path.join(cwd, "..", "wavecal", fname)
         return fname
